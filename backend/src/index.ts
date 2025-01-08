3f00465d1de0c8665a15edba2f2f993caec7a318
Aashish Panthee <aashishpanthee060@gmail.com>
2025-01-08 15:36:25 +0545

    feat: add trpc package
    - RPC is short for "Remote Procedure Call". It is a way of calling functions on one computer (the server) from another computer (the client). With traditional HTTP/REST APIs, you call a URL and get a response. With RPC, you call a function and get a response.   tRPC (TypeScript Remote Procedure Call) is one implementation of RPC, designed for TypeScript monorepos.   - add getIdeas function & integrated it with express server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,31 +1,15 @@
 import express from "express";
+import * as trpcExpress from "@trpc/server/adapters/express";
+import { trpcRouter } from './trpc';
 const expressApp = express();
-
-  const ideas = [
-    {
-      id: 1,
-      title: 'Idea 1',
-      description: 'Idea 1 description',
-    },
-    {
-      id: 2,
-      title: 'Idea 2',
-      description: 'Idea 2 description',
-    },
-    {
-      id: 3,
-      title: 'Idea 3',
-      description: 'Idea 3 description',
-    },
-  ]
 
 expressApp.get("/ping", (req, res) => {
   res.send('pong')
 })
 
-expressApp.get('/ideas', (req, res) => {
-  res.send(ideas)
-})
+expressApp.use('/trpc', trpcExpress.createExpressMiddleware({
+  router: trpcRouter
+}))
 
 expressApp.listen(3000, () => {
   console.info("Server is listening on port 3000")
