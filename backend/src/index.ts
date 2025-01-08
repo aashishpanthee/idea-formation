9743963229d8e49b77ff441df08e868e6197f15e
Aashish Panthee <aashishpanthee060@gmail.com>
2025-01-08 14:42:42 +0545

    chore: add express package
    - add express package & @types/express  for type definitions of express.  - created an endpoint for testing

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,10 @@
-console.log('hello world')
+import express from "express";
+const expressApp = express();
+
+expressApp.get("/ping", (req, res) => {
+  res.send('pong')
+})
+
+expressApp.listen(3000, () => {
+  console.info("Server is listening on port 3000")
+})
