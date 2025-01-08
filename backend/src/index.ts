be335c934c8775ff55a896cd26cf4828ec10091b
Aashish Panthee <aashishpanthee060@gmail.com>
2025-01-08 15:15:52 +0545

    feat: add new route for testing

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,30 @@
 import express from "express";
 const expressApp = express();
 
+  const ideas = [
+    {
+      id: 1,
+      title: 'Idea 1',
+      description: 'Idea 1 description',
+    },
+    {
+      id: 2,
+      title: 'Idea 2',
+      description: 'Idea 2 description',
+    },
+    {
+      id: 3,
+      title: 'Idea 3',
+      description: 'Idea 3 description',
+    },
+  ]
+
 expressApp.get("/ping", (req, res) => {
   res.send('pong')
+})
+
+expressApp.get('/ideas', (req, res) => {
+  res.send(ideas)
 })
 
 expressApp.listen(3000, () => {
