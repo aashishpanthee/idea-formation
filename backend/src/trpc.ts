52ced273045a6a10e35c7c70981e40e055307465
Aashish Panthee <aashishpanthee060@gmail.com>
2025-01-14 15:45:14 +0545

    Feat: create getIdea route for individual idea page

--- backend/src/trpc.ts
+++ backend/src/trpc.ts
@@ -1,11 +1,12 @@
 import { initTRPC } from '@trpc/server'
 import _ from 'lodash'
+import { z } from 'zod'
 
 const ideas = _.times(50, (i) => ({
   id: `${i}`,
   title: `Idea ${i}`,
   description: `Idea ${i} description`,
-  text: _.times(50, (j) => `Idea ${i} text ${j}`).join(''),
+  text: _.times(50, (j) => `<p>Text paragrph ${j} of idea ${i}...</p>`).join('\n'),
 }))
 
 const trpc = initTRPC.create()
@@ -14,6 +15,16 @@ export const trpcRouter = trpc.router({
   getIdeas: trpc.procedure.query(() => {
     return { ideas: ideas.map((idea) => _.pick(idea, ['id', 'title', 'description'])) }
   }),
+  getIdea: trpc.procedure
+    .input(
+      z.object({
+        ideaId: z.string(),
+      })
+    )
+    .query(({ input }) => {
+      const idea = ideas.find((idea) => idea.id === input.ideaId)
+      return { idea: idea || null }
+    }),
 })
 
 export type TrpcRouter = typeof trpcRouter
--- web-app/src/pages/ViewIdeaPage/index.tsx
+++ web-app/src/pages/ViewIdeaPage/index.tsx
@@ -1,17 +1,24 @@
 import { useParams } from 'react-router-dom'
 import { ViewIdeaRouteParams } from '../../lib/routes'
+import { trpc } from '../../lib/trpc'
 
 function ViewIdeaPage() {
   const { ideaId } = useParams() as ViewIdeaRouteParams
+  const { data, error, isLoading, isError } = trpc.getIdea.useQuery({ ideaId })
+  if (isLoading) {
+    return <span>Loading.....</span>
+  }
+  if (isError) {
+    return <span>Error: {error.message}</span>
+  }
+  if (!data.idea) {
+    return <span>Idea not found</span>
+  }
   return (
     <div>
-      <h1>{ideaId}</h1>
-      <p>Description of Idea 1</p>
-      <div>
-        <p>Text paragraph of idea 1</p>
-        <p>Text paragraph of idea 2</p>
-        <p>Text paragraph of idea 3</p>
-      </div>
+      <h1>{data.idea.title}</h1>
+      <p>{data.idea.description}</p>
+      <div dangerouslySetInnerHTML={{ __html: data.idea.text }} />
     </div>
   )
 }
